71deb20391e80c51d771f6c3cf81d44e0000e328
ive <ivecloudblack@gmail.com>
2022-01-01 16:22:06 +0900

    Edit github id.

--- 3-jenkins-job-dsl/nodejsdocker.groovy
+++ 3-jenkins-job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/yslee/docker-demo-toyproj.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/seyslee/docker-demo-toyproj.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
